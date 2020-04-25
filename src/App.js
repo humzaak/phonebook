854216a7af45b36bfa872e9b4d44749e159b7b20
Humza Akhtar <humzaakhtar1@gmail.com>
2020-04-25 22:26:42 +0800

    added db.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Data from './components/Data'
 import Filter from './components/Filter'
 import List from './components/List'
@@ -13,13 +14,17 @@ const App = () => {
   const [ filterValue, setFilterValue ] = useState('')
 
 
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
 
 
